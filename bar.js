b079ec0b0ad8a64ec89386337a37036fd86d6093
Romain Capocasale <romain.capocasale@he-arc.ch>
2019-05-01 08:52:11 +0200

    add bar and graph class

---
+++ bar.js
@@ -0,0 +1,15 @@
+class Bar {
+  constructor(id, name, avgPriceDrink, populatity){
+    this.id = id;
+    this.name = name;
+    this.avgPriceDrink = avgPriceDrink;
+    this.populatity = populatity;
+    this.visited = false;
+    this.meeted = false;
+    this.neighboors = [];
+  }
+
+  addNeigthboors(bar){
+    this.neighboors.push(bar);
+  }
+}
---
+++ script.js
@@ -0,0 +1,50 @@
+// create an array with nodes
+var nodes = new vis.DataSet([
+{id: 1, label: 'Charlot', x: 100, y: 0},
+{id: 2, label: 'Brasseur', x: 300, y: 0},
+{id: 3, label: '21', x: 650, y: 0},
+{id: 4, label: 'King Du Lac', x: 400, y: 60},
+{id: 5, label: 'Waves', x: 500, y: 65}
+]);
+
+// create an array with edges
+var edges = new vis.DataSet([
+{from: 1, to: 2, label: 100 },
+{from: 1, to: 3, label:  550},
+{from: 1, to: 4, label:  400},
+{from: 1, to: 5, label:  550},
+
+{from: 2, to: 3, label: 450 },
+{from: 2, to: 4, label:  200},
+{from: 2, to: 5, label:  250},
+
+{from: 3, to: 4, label: 250 },
+{from: 3, to: 5, label: 150},
+
+{from: 4, to:5, label: 100}
+]);
+
+// create a network
+var container = document.getElementById('mynetwork');
+
+// provide the data in the vis format
+var data = {
+    nodes: nodes,
+    edges: edges
+};
+
+var options = {
+   physics: false,
+   interaction: {
+      dragNodes: false,// do not allow dragging nodes
+    },
+   edges: {
+     hoverWidth:3,
+   }
+};
+
+// initialize your network!
+var network = new vis.Network(container, data, options);
+
+let g = new Graph();
+g.loadBarsFromJson(nodes);
